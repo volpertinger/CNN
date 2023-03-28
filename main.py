37959db357b99d8350884eef8c1d4dd4e40d6c06
Nikolay <merzlovnik@mail.ru>
2023-03-28 23:48:21 +0300

    Codestyle

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
 
     model = cnn.Model(cc.DATASET, img_proc, cc.CLASSES_NUMBER, cc.INPUT_SHAPE, cc.BATCH_SIZE, cc.EPOCHS,
                       cc.SAVE_FILENAME, cc.TRAIN_HISTORY_FILENAME, cc.COMMON_ACTIVATION, cc.LAST_ACTIVATION,
-                      cc.KERNEL_INITIALIZER, cc.LEARNING_RATE, cc.MOMENTUM, cc.LOSS, cc.METRICS, cc.SMALL_DOT,
-                      cc.LARGE_DOT, cc.SMALL_DENSE, cc.LARGE_DENSE, cc.SMALL_FILTER, cc.LARGE_FILTER, cc.CMAP,
-                      cc.POSSIBILITY_PRECISION, cc.WITH_INFO, cc.VERBOSE)
+                      cc.KERNEL_INITIALIZER, cc.LEARNING_RATE, cc.MOMENTUM, cc.LOSS, cc.VALIDATION_SPLIT, cc.METRICS,
+                      cc.SMALL_DOT, cc.LARGE_DOT, cc.SMALL_DENSE, cc.LARGE_DENSE, cc.SMALL_FILTER, cc.LARGE_FILTER,
+                      cc.CMAP, cc.POSSIBILITY_PRECISION, cc.WITH_INFO, cc.WITH_PLOT, cc.VERBOSE)
     model.train()
     telegram_bot = ss.TelegramBot(model)
     telegram_bot.start()
